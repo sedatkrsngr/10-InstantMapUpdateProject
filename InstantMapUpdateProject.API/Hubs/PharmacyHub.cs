80114d4f7abf3fb48672fa3927c52bcdbdc67130
sedat karasungur <limitless_fearless@hotmail.com>
2021-05-24 09:47:16 +0300

    Son

--- InstantMapUpdateProject.API/Hubs/PharmacyHub.cs
+++ InstantMapUpdateProject.API/Hubs/PharmacyHub.cs
@@ -30,6 +30,27 @@ namespace InstantMapUpdateProject.API.Hubs
 
         }
 
+        public async Task GetLocationMobile(CourierLocation courier)
+        {
+            await Task.Delay(500);
+
+            await Clients.Group(courier.RoomId).SendAsync("mobileClient", courier.courierLatitude, courier.courierLongitude);
+
+            OrderFollowCoordinate order = new OrderFollowCoordinate();
+            order.courierLatitude = courier.courierLatitude;
+            order.courierLongitude = courier.courierLongitude;
+            order.userLatitude = courier.userLatitude;
+            order.userLongitude = courier.userLongitude;
+            order.centerLatitude = courier.centerLatitude;
+            order.centerLongitude = courier.centerLongitude;
+            order.CourierId = courier.CourierId;
+            order.RoomId = courier.RoomId;
+
+            await _context.OrderFollowCoordinates.AddAsync(order);
+            await _context.SaveChangesAsync();
+
+        }
+
         public async Task addGroup(string grupName)//hub üzerinde olduğu için bu metodu invoke etmek gerekiyor
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, grupName);//connectionid her sayfa yenilendiğinde değişiyor          
